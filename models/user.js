5fa1b71feb9dc19ee18a05b730b930c7e214980c
Sohail Faiz Rasool <104111335+SohailFaizRasool@users.noreply.github.com>
2024-03-26 03:37:39 +0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose")
-
 const {Schema } = mongoose;
 
+const encrypt = require("mongoose-encryption")
+const MY_SECRET_KEY= "thisismysecret"
 
-const userSchema= new Schema({
+
+// const userSchema= new Schema({
+const userSchema= new mongoose.Schema({
     email: String,
     password: String,
 })
+ 
+// userSchema.plugin(encrypt,{secret:MY_SECRET_KEY})//this encrypt all the database
+userSchema.plugin(encrypt,{secret:MY_SECRET_KEY , encryptedFields:["password"]} )//this encrypt only certine fields
 
+//these process incrypt while save and decrypt while find() all other code work same
 module.exports = mongoose.model("User",userSchema)
